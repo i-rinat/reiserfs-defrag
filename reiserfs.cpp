838bebbd2e01932498250f565dabdc6de477fc65
Rinat <ibragimovrinat@mail.ru>
2012-08-23 20:01:12 +0400

    superblock reading stub

--- reiserfs.cpp
+++ reiserfs.cpp
@@ -1,5 +1,9 @@
 #include "reiserfs.hpp"
 #include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 
 ReiserFs::ReiserFs()
 {
@@ -16,14 +20,37 @@ ReiserFs::open(std::string name)
 {
     std::cout << "open " << name << std::endl;
     this->fname = name;
+    fd = ::open(name.c_str(), O_RDWR | O_SYNC | O_LARGEFILE);
+    if (-1 == fd) {
+        std::cerr << "error: can't open file `" << name << "', errno = " << errno << std::endl;
+        return RFSD_FAIL;
+    }
 
-    this->readSuperblock(this->sb);
+    this->readSuperblock();
+    return RFSD_OK;
 }
 
 void
-ReiserFs::readSuperblock(FsSuperblock &sb)
+ReiserFs::readSuperblock()
 {
     std::cout << "readSuperblock" << std::endl;
+    const off_t sb_offset = 65536;
+    off_t res = ::lseek (this->fd, sb_offset, SEEK_SET);
+    if (res != sb_offset) {
+        std::cerr << "error: can't lseek, errno = " << errno << std::endl;
+        return;
+    }
+
+    char buf[4096];
+    ssize_t bytes_read = ::read (this->fd, buf, sizeof(buf));
+    if (bytes_read == -1) {
+        std::cerr << "error: can't read, errno = " << errno << ", " << strerror(errno) << std::endl;
+        std::cout << sizeof(buf) << std::endl;
+        std::cout << this->fd << std::endl;
+        return;
+    }
+
+    memcpy(&this->sb, buf, sizeof(this->sb));
 }
 
 int
--- reiserfs.hpp
+++ reiserfs.hpp
@@ -1,11 +1,58 @@
+#define _FILE_OFFSET_BITS 64
 #include <stdint.h>
 #include <string>
 #include <map>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define RFSD_OK     0
+#define RFSD_FAIL   -1
 
 class FsJournal {
 public:
 
 };
+
+struct FsSuperblock {
+    uint32_t s_block_count;
+    uint32_t s_free_blocks;
+    uint32_t s_root_block;
+
+    // from struct journal_params
+    uint32_t jp_journal_1st_block;
+    uint32_t jp_journal_dev;
+    uint32_t jp_journal_size;
+    uint32_t jp_journal_trans_max;
+    uint32_t jp_journal_magic;
+    uint32_t jp_journal_max_batch;
+    uint32_t jp_journal_max_commit_age;
+    uint32_t jp_journal_max_trans_age;
+
+    uint16_t s_blocksize;
+    uint16_t s_oid_maxsize;
+    uint16_t s_oid_cursize;
+    uint16_t s_umount_state;
+    char s_magic[10];
+    uint16_t s_fs_state;
+    uint16_t s_hash_function_code;
+    uint16_t s_tree_height;
+    uint16_t s_bmap_nr;
+    uint16_t s_version;
+    uint16_t s_reserved_for_journal;
+
+    // end of v1 superblock, v2 additions below
+    uint32_t s_inode_generation;
+    uint32_t s_flags;
+    uint8_t s_uuid[16];
+    char s_label[16];
+    uint16_t s_mnt_count;
+    uint16_t s_max_mnt_count;
+    uint32_t s_lastcheck;
+    uint32_t s_check_interval;
+    uint8_t s_unused[76];
+
+} __attribute__ ((__packed__));
 
 class Block {
 public:
@@ -20,10 +67,6 @@ public:
 };
 
 class FsBitmap {
-public:
-};
-
-class FsSuperblock {
 public:
 };
 
@@ -45,6 +88,7 @@ private:
     FsJournal journal;
     FsSuperblock sb;
     std::string fname;
+    int fd;
 
-    void readSuperblock(FsSuperblock &sb);
+    void readSuperblock();
 };
