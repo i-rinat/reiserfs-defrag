249a615963ae9248979d8d3bf2adffc07698477a
Rinat <ibragimovrinat@mail.ru>
2012-09-01 22:38:21 +0400

    misc updates

--- defrag.cpp
+++ defrag.cpp
@@ -6,8 +6,15 @@ main (int argc, char *argv[])
     ReiserFs fs;
 
     fs.open("../image/reiserfs.image");
+    //fs.looseWalkTree();
     fs.printFirstFreeBlock();
-    fs.moveBlock(250000, 8211);
+
+    // move last used to first free
+    uint32_t bid1 = fs.findFreeBlockAfter(16+1+8192+1);
+    uint32_t bid2 = fs.sizeInBlocks();
+    while (bid2 > 0 && !fs.blockUsed(bid2)) bid2 --;
+
+    fs.moveBlock(bid2, bid1);
     fs.close();
 
     return 0;
