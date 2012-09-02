74d67634701574d8b28606bbb429a4e2ea791296
Rinat <ibragimovrinat@mail.ru>
2012-09-03 01:16:31 +0400

    misc code: moveback -- try to move all movable blocks to end of an FS.

---
+++ misc/moveback.cpp
@@ -0,0 +1,31 @@
+#include "../reiserfs.hpp"
+#include <stdio.h>
+
+int
+main (int argc, char *argv[])
+{
+    ReiserFs fs;
+    fs.open("../image/reiserfs.image", false);
+
+    std::map<uint32_t, uint32_t> movemap;
+    uint32_t freeblock = fs.sizeInBlocks();
+
+    uint32_t block_idx = 16 + 1 + 8192 + 1 + 1;
+
+    while (block_idx < freeblock) {
+        if (block_idx % BLOCKS_PER_BITMAP == 0) {
+            block_idx ++; continue;
+        }
+        if (fs.blockUsed(block_idx)) {
+            freeblock = fs.findFreeBlockBefore(freeblock);
+            if (block_idx < freeblock)
+                movemap[block_idx] = freeblock;
+        }
+        block_idx ++;
+    }
+
+    fs.moveMultipleBlocks(movemap);
+    fs.close();
+    return 0;
+}
+
