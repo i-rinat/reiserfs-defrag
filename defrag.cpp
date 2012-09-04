29a95da2c0f93d508988432d7c93b07e9531291b
Rinat <ibragimovrinat@mail.ru>
2012-09-04 20:14:28 +0400

    saving progress: removed code from defrag.cpp

--- defrag.cpp
+++ defrag.cpp
@@ -4,18 +4,8 @@ int
 main (int argc, char *argv[])
 {
     ReiserFs fs;
-
     fs.open("../image/reiserfs.image");
-    //fs.looseWalkTree();
-    fs.printFirstFreeBlock();
 
-    // move last used to first free
-    uint32_t bid1 = fs.findFreeBlockAfter(16+1+8192+1);
-    uint32_t bid2 = fs.sizeInBlocks();
-    while (bid2 > 0 && !fs.blockUsed(bid2)) bid2 --;
-
-    fs.moveBlock(bid2, bid1);
     fs.close();
-
     return 0;
 }
